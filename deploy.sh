e18ad1445fa795d54fa98ce2d9697acd5fed6c53
Anand <appurajacool2015@gmail.com>
2022-07-27 05:58:07 +0000

    Adding EKS kubeconfig and pull image everytime even if the image name is same

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,25 @@ docker build -t appurajacool2015/ekspulumispringbootpostgresrestapidemo .
 # we can docker login as a initial step
 #docker login
 docker push appurajacool2015/ekspulumispringbootpostgresrestapidemo
-kubectl apply -f postgres-secrets.yaml
-kubectl apply -f postgres-storage.yaml
-kubectl apply -f postgres-deployment.yaml
-kubectl apply -f postgres-service.yaml
-kubectl delete config map hostname-config
-kubectl create configmap hostname-config --from-literal=postgres_host=$(kubectl get svc postgres -o jsonpath="{.spec.clusterIP}")
-kubectl apply -f springboot-deployment.yaml
-kubectl apply -f springboot-service.yaml
+KUBECONFIG=~/EKS-kubeconfig kubectl apply -f postgres-secrets.yaml
+KUBECONFIG=~/EKS-kubeconfig kubectl apply -f postgres-storage.yaml
+KUBECONFIG=~/EKS-kubeconfig kubectl apply -f postgres-deployment.yaml
+KUBECONFIG=~/EKS-kubeconfig kubectl apply -f postgres-service.yaml
+KUBECONFIG=~/EKS-kubeconfig kubectl delete configmap hostname-config
+KUBECONFIG=~/EKS-kubeconfig kubectl create configmap hostname-config --from-literal=postgres_host=$(KUBECONFIG=~/EKS-kubeconfig kubectl get svc postgres -o jsonpath="{.spec.clusterIP}")
+KUBECONFIG=~/EKS-kubeconfig kubectl apply -f springboot-deployment.yaml
+KUBECONFIG=~/EKS-kubeconfig kubectl apply -f springboot-service.yaml
+
+################################################################################
+# Commenting the below code to as we need different KUBECONFIG for EKS and GKE #
+################################################################################
+#kubectl apply #f postgres#secrets.yaml
+#kubectl apply #f postgres#storage.yaml
+#kubectl apply #f postgres#deployment.yaml
+#kubectl apply #f postgres#service.yaml
+#kubectl delete config map hostname#config
+#kubectl create configmap hostname#config #-from-literal=postgres_host=$(kubectl get svc postgres -o jsonpath="{.spec.clusterIP}")
+#kubectl apply -f springboot-deployment.yaml
+#kubectl apply -f springboot-service.yaml
+
+
